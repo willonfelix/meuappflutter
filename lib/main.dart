70309e27a9ceda55f226da2384dc6ec4975ece19
Wiilon Felix <willon.felix@gmail.com>
2024-04-10 00:01:02 -0300

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -20,9 +20,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Meu Primeiro Projeto Flutter'),
     );
   }
 }
@@ -94,12 +94,17 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'Clique no botão abaixo para testa o app',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            Text(
+              'Quantidade de cliques',
+              style: Theme.of(context).textTheme.headline5,
             ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
-            ),
+            )
           ],
         ),
       ),
